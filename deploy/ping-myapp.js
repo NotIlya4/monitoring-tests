3dcc9d0606dc6731e9c1eb8160519632150e60bf
Ilya Krupcov <secretman44444@gmail.com>
2023-11-04 02:27:53 +0300

    Update

--- deploy/ping-myapp.js
+++ deploy/ping-myapp.js
@@ -2,6 +2,6 @@ import http from 'k6/http';
 import { sleep } from 'k6';
 
 export default function () {
-  http.get('http://localhost:5000/weather');
+  http.get('http://localhost:5000/weather?number=100');
   sleep((Math.random() * 0.25) * 2);
 } 
